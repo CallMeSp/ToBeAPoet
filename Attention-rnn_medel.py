1ef244447d13f0623214bd6fb059eeb41fd873a0
sp <995199235@qq.com>
2019-03-04 20:56:51 +0800

    final

--- Attention-rnn_medel.py
+++ Attention-rnn_medel.py
@@ -376,5 +376,5 @@ if __name__ == '__main__':
     # Learning Rate
     learning_rate = 0.001
     print('modified attention start....')
-    # predict('小草偷偷地从土里钻出来，嫩嫩的，绿绿的。园子里，田野里，瞧去，一大片一大片满是的。坐着，躺着，打两个滚，踢几脚球，赛几趟跑，捉几回迷藏。风轻悄悄的，草软绵绵的。')
-    train_attention_modified()
+    predict('小草偷偷地从土里钻出来，嫩嫩的，绿绿的。园子里，田野里，瞧去，一大片一大片满是的。坐着，躺着，打两个滚，踢几脚球，赛几趟跑，捉几回迷藏。风轻悄悄的，草软绵绵的。')
+    # train_attention_modified()
