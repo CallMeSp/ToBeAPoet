9bf4db174f27a6dd82653cf3cd4ac271dd1dcfdb
sp <995199235@qq.com>
2019-03-04 15:11:47 +0800

    delete useless code

--- Attention-rnn_medel.py
+++ Attention-rnn_medel.py
@@ -8,25 +8,6 @@ import myDataUtil
 from word2vecHelper import extract_character_vocab, get_batches, getbatches_modified, genwordEmbedding
 
 
-def get_inputs():
-    '''
-    模型输入tensor
-    '''
-    inputs = tf.placeholder(tf.int32, [None, None], name='inputs')
-    targets = tf.placeholder(tf.int32, [None, None], name='targets')
-    learning_rate = tf.placeholder(tf.float32, name='learning_rate')
-
-    # 定义target序列最大长度（之后target_sequence_length和source_sequence_length会作为feed_dict的参数）
-    target_sequence_length = tf.placeholder(
-        tf.int32, (None,), name='target_sequence_length')
-    max_target_sequence_length = tf.reduce_max(
-        target_sequence_length, name='max_target_len')
-    source_sequence_length = tf.placeholder(
-        tf.int32, (None,), name='source_sequence_length')
-
-    return inputs, targets, learning_rate, target_sequence_length, max_target_sequence_length, source_sequence_length
-
-
 def get_inputs_modified():
     '''
     模型输入tensor
@@ -46,42 +27,6 @@ def get_inputs_modified():
 
     return input_keywords_ids, input_pretexts_ids, targets, learning_rate, target_sequence_length, \
            max_target_sequence_length, input_keywords_length, input_pretexts_length
-
-
-def get_encoder_layer(input_data, rnn_size, num_layers, source_sequence_length, source_vocab_size,
-                      encoding_embedding_size):
-    '''
-
-    :param input_data: 输入tensor
-    :param rnn_size: rnn隐层结点数量
-    :param num_layers: 堆叠的rnn cell数量
-    :param source_sequence_length: 源数据的序列长度
-    :param source_vocab_size: 源数据的词典大小
-    :param encoding_embedding_size: embedding的大小
-    :return: encoder_output, encoder_state
-    '''
-    # encoder_embedding
-    encoder_embeddings = tf.Variable(tf.random_uniform([source_vocab_size, decoding_embedding_size]))
-    decoder_embed_input = tf.nn.embedding_lookup(encoder_embeddings, input_data)
-
-    encoder_embed_input = tf.contrib.layers.embed_sequence(input_data, source_vocab_size, encoding_embedding_size)
-
-    #  rnn cell
-    def get_lstm_cell(rnn_size):
-        lstm_cell = tf.contrib.rnn.LSTMCell(rnn_size, initializer=tf.random_uniform_initializer(-0.1, 0.1, seed=2))
-        return lstm_cell
-
-    encoder_fw_cell = get_lstm_cell(rnn_size)
-    encoder_bw_cell = get_lstm_cell(rnn_size)
-    (encoder_fw_outputs, encoder_bw_outputs), (encoder_fw_state, encoder_bw_state) = tf.nn.bidirectional_dynamic_rnn(
-        cell_fw=encoder_fw_cell, cell_bw=encoder_bw_cell, inputs=encoder_embed_input,
-        sequence_length=source_sequence_length, dtype=tf.float32, time_major=False)
-    # shape:[batch_size,max_length,rnn_size]
-    encoder_outputs = tf.concat((encoder_fw_outputs, encoder_bw_outputs), 2)
-    encoder_final_state_c = tf.concat((encoder_fw_state.c, encoder_bw_state.c), 1)
-    encoder_final_state_h = tf.concat((encoder_fw_state.h, encoder_bw_state.h), 1)
-    encoder_final_state = tf.contrib.rnn.LSTMStateTuple(c=encoder_final_state_c, h=encoder_final_state_h)
-    return encoder_outputs, encoder_final_state
 
 
 def get_encoder_layer_modified(batch_keyword_ids, batch_pretext_ids, rnn_size, num_layers, keywords_length,
@@ -219,23 +164,6 @@ def decoding_layer(target_letter_to_int, decoding_embedding_size, num_layers, rn
     return training_decoder_output, predict_decoder_output
 
 
-def seq2seq_model(input_data, targets, lr, target_sequence_length, max_target_sequence_length, source_sequence_length,
-                  source_vocab_size, target_vocab_size, encoder_embedding_size, decoder_embedding_size, rnn_size,
-                  num_layers):
-    # get state of encoder
-    encoder_output, encoder_state = get_encoder_layer(input_data, rnn_size, num_layers, source_sequence_length,
-                                                      source_vocab_size,
-                                                      encoder_embedding_size)
-    # the input of decoder which processed
-    decoder_input = process_decoder_input(targets, word2id, batch_size)
-    training_decoder_output, predicting_decoder_output = decoding_layer(word2id, decoding_embedding_size,
-                                                                        num_layers, rnn_size, target_sequence_length,
-                                                                        source_sequence_length,
-                                                                        max_target_sequence_length, encoder_output,
-                                                                        decoder_input)
-    return training_decoder_output, predicting_decoder_output
-
-
 def seq2seq_model_modified(keywords_ids, pretexts_ids, targets, lr, target_sequence_length, max_target_sequence_length,
                            keywords_sequence_length, pretexts_sequence_length, source_vocab_size, target_vocab_size,
                            encoder_embedding_size, decoder_embedding_size, rnn_size, num_layers):
@@ -263,90 +191,6 @@ def source_to_seq(text):
     sequence_length = 10
     return [word2id.get(word, word2id['<UNK>']) for word in text] + [
         word2id['<PAD>']] * (sequence_length - len(text))
-
-
-# Attention 版本的train
-def train_attention():
-    # 构造graph
-    train_graph = tf.Graph()
-    with train_graph.as_default():
-        # 获得模型输入
-
-        input_data, targets, lr, target_sequence_length, max_target_sequence_length, source_sequence_length = get_inputs(
-        )
-        training_decoder_output, predict_output = seq2seq_model(
-            input_data, targets, lr, target_sequence_length,
-            max_target_sequence_length, source_sequence_length,
-            len(word2id), len(word2id),
-            encoding_embedding_size, decoding_embedding_size, rnn_size, num_layers)
-        training_logits = tf.identity(training_decoder_output.rnn_output, 'logits')
-        predicting_logits = tf.identity(
-            predict_output.sample_id, name='predictions')
-        masks = tf.sequence_mask(
-            target_sequence_length,
-            max_target_sequence_length,
-            dtype=tf.float32,
-            name='masks')
-        with tf.name_scope("optimization"):
-            # Loss function
-            cost = tf.contrib.seq2seq.sequence_loss(training_logits, targets,
-                                                    masks)
-            # Optimizer
-            optimizer = tf.train.AdamOptimizer(lr)
-            # Gradient Clipping
-            gradients = optimizer.compute_gradients(cost)
-            capped_gradients = [(tf.clip_by_value(grad, -5., 5.), var)
-                                for grad, var in gradients if grad is not None]
-            train_op = optimizer.apply_gradients(capped_gradients)
-    # 将数据集分割为train和validation
-    train_source = keywords_int[50 * batch_size:]
-    train_target = curlines_int[50 * batch_size:]
-    # 留出一个batch进行验证
-    valid_source = keywords_int[:50 * batch_size]
-    valid_target = curlines_int[:50 * batch_size]
-    (valid_targets_batch, valid_sources_batch, valid_targets_lengths, valid_sources_lengths) = next(
-        get_batches(valid_target, valid_source, batch_size,
-                    word2id['<PAD>'],
-                    word2id['<PAD>']))
-    display_step = 50  # 每隔50轮输出loss
-    checkpoint = "./model/trained_model_attention.ckpt"
-    with tf.Session(graph=train_graph) as sess:
-        sess.run(tf.global_variables_initializer())
-        for epoch_i in range(1, epochs + 1):
-            for batch_i, (targets_batch, sources_batch, targets_lengths, sources_lengths) in enumerate(
-                    get_batches(train_target, train_source, batch_size,
-                                word2id['<PAD>'],
-                                word2id['<PAD>'])):
-                _, loss = sess.run(
-                    [train_op, cost],
-                    {input_data: sources_batch,
-                     targets: targets_batch,
-                     lr: learning_rate,
-                     target_sequence_length: targets_lengths,
-                     source_sequence_length: sources_lengths})
-
-                if batch_i % display_step == 0:
-                    # 计算validation loss
-                    validation_loss = sess.run(
-                        [cost],
-                        {input_data: valid_sources_batch,
-                         targets: valid_targets_batch,
-                         lr: learning_rate,
-                         target_sequence_length: valid_targets_lengths,
-                         source_sequence_length: valid_sources_lengths})
-
-                    print('Epoch {:>3}/{} Batch {:>4}/{} - Training Loss: {:>6.3f}  - Validation loss: {:>6.3f}'
-                          .format(epoch_i,
-                                  epochs,
-                                  batch_i,
-                                  len(train_source) // batch_size,
-                                  loss,
-                                  validation_loss[0]))
-
-        # 保存模型
-        saver = tf.train.Saver()
-        saver.save(sess, checkpoint)
-        print('Model Trained and Saved')  # 构造graph
 
 
 # modified Attention 版本的train
@@ -439,8 +283,8 @@ def train_attention_modified():
 def predict(input_sentence):
     # 输入一个单词
     # user_keywords = myDataUtil.extractKeywordFromUser(input_sentence, 4)
-    user_keywords = ['春天','桃花','美丽','生机']
-    print('keywords:',user_keywords)
+    user_keywords = ['树', '土', '黄花', '白云']
+    print('keywords:', user_keywords)
     text = [source_to_seq(word) for word in input_sentence]
     checkpoint = "./model/trained_model_attention.ckpt"
 
@@ -472,7 +316,7 @@ def predict(input_sentence):
         target_sequence_length = loaded_graph.get_tensor_by_name('target_sequence_length:0')
         print(target_sequence_length)
         testpretexts = [[]]
-        testpretexts_length = [0,5,11,17]
+        testpretexts_length = [0, 5, 11, 17]
         for i in range(4):
             answer_logits = sess.run(logits, {input_keyword: [text[i]] * batch_size,
                                               input_pretext: [source_to_seq(testpretexts[i])] * batch_size,
@@ -482,7 +326,7 @@ def predict(input_sentence):
             pad = word2id["<PAD>"]
             responseIds = [i for i in answer_logits if i != pad]
             responseWords = " ".join([id2word[i] for i in answer_logits if i != pad])
-            if not i==0:
+            if not i == 0:
                 testpretexts.append(testpretexts[i] + ',' + responseWords)
             else:
                 testpretexts.append(responseWords)
@@ -490,18 +334,6 @@ def predict(input_sentence):
 
         print('原始输入:', 'keyword:', user_keywords)
         print('Response : {}'.format(testpretexts[-1]))
-
-    # pad = word2id["<PAD>"]
-    #
-    # print('原始输入:', 'keyword:',input_word,',pretext:',input_pretext)
-    #
-    # print('\nSource')
-    # print('  Word 编号:    {}'.format([i for i in text]))
-    # print('  Input Words: {}'.format(" ".join([id2word[i] for i in text])))
-    #
-    # print('\nTarget')
-    # print('  Word 编号:       {}'.format([i for i in answer_logits if i != pad]))
-    # print('  Response Words: {}'.format(" ".join([id2word[i] for i in answer_logits if i != pad])))
 
 
 if __name__ == '__main__':
@@ -533,7 +365,7 @@ if __name__ == '__main__':
     # Number of Epochs
     epochs = 30
     # Batch Size
-    batch_size = 64
+    batch_size = 256
     # RNN Size
     rnn_size = 512
     # Number of Layers
